ff2cfc71b6ac0315cd6815edaa7eb5a8903ce04f
kanekotic <alvarojosepl@gmail.com>
2018-07-21 14:32:20 +0300

    added buttons for clean and report

--- app/components/main.js
+++ app/components/main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
-import { View, Image } from 'react-native'
-import { Container } from 'native-base'
+import { View, Image, Text } from 'react-native'
+import { Container, Button } from 'native-base'
 import header from './common/header'
 
 
@@ -13,9 +13,13 @@ export default class Main extends Component {
         return (
             <Container style={{ backgroundColor: 'white' }}> 
                 {header("Cleanly")}
-                <View>
-                <Image  style={{ width:370, resizeMode: 'center', marginTop: 20, marginBottom: 20, backgroundColor: 'transparent' }} 
+                <View style={{ flexDirection: "row", flex: 1}}>
+                    <Image style={{ width:370, resizeMode: 'center', marginTop: 20, marginBottom: 20, backgroundColor: 'transparent' }} 
                         source={require('./resources/logo.png')} />
+                </View>
+                <View style={{ flexDirection: "row", flex: 1, position: "absolute", bottom: 50, left: 0, right: 0, padding: 15 }}>
+                <Button success><Text> Report </Text></Button>
+                <Button success><Text> Clean </Text></Button>
                 </View>
             </Container>
         )

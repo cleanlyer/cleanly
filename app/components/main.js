b930db40c7a93554bca6b6680e45a627c07d41a5
kanekotic <alvarojosepl@gmail.com>
2018-07-22 12:45:13 +0300

    improved first page

--- app/components/main.js
+++ app/components/main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { View, Image, Text } from 'react-native'
-import { Container, Button } from 'native-base'
+import { Container, Button, Thumbnail } from 'native-base'
 import header from './common/header'
 import footer from './common/footer'
 import { connect } from 'react-redux'
@@ -11,12 +11,20 @@ class Main extends Component {
     }
 
     render() {
+        score = this.props.score
         return (
             <Container style={{ backgroundColor: 'white' }}> 
                 {header("Cleanly", this.props.score)}
+                <View style={{flex: 1, alignItems:'center'}}>
+                    <Thumbnail large style={{ resizeMode: 'center', marginTop: 20, marginBottom: 20, backgroundColor: 'transparent' }} 
+                        source={require('./resources/me.png')} />
+                        
+                        <Text style={{fontWeight: "bold", marginTop: 20, marginBottom: 20}}>{score} kudos</Text>
+                        <Text>Go and safe the planet Adrien!</Text>
+                </View>
                 <View style={{flex: 1}}>
-                    <Image style={{ width:370, resizeMode: 'center', marginTop: 20, marginBottom: 20, backgroundColor: 'transparent' }} 
-                        source={require('./resources/logo.png')} />
+                </View>
+                <View style={{flex: 3}}>
                 </View>
                 {footer()}
             </Container>

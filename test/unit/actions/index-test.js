07c72ea3ab1c145ce492fca2b23436730e383495
kanekotic <alvarojosepl@gmail.com>
2018-07-25 07:37:55 +0200

    added test to actions index

---
+++ test/unit/actions/index-test.js
@@ -0,0 +1,22 @@
+import { ActionCreators } from '../../../src/actions'
+describe('index actions should have', () => {
+    it('updateScore Action', () => {
+    expect(ActionCreators.updateScore).toBeInstanceOf(Function)
+    })
+
+    it('updateUserCoordinates Action', () => {
+    expect(ActionCreators.updateUserCoordinates).toBeInstanceOf(Function)
+    })
+
+    it('updateGarbageList Action', () => {
+    expect(ActionCreators.updateGarbageList).toBeInstanceOf(Function)
+    })
+
+    it('sendReport Action', () => {
+    expect(ActionCreators.sendReport).toBeInstanceOf(Function)
+    })
+
+    it('sendClean Action', () => {
+    expect(ActionCreators.sendClean).toBeInstanceOf(Function)
+    })
+})

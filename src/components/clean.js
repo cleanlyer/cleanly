8a728ec4efea0ea8dd61d1725d0cb743e16fe9ed
kanekotic <alvarojosepl@gmail.com>
2018-08-05 13:58:32 +0200

    fix map resolution

--- src/components/clean.js
+++ src/components/clean.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
-import { List, ListItem, Body, Right, Icon, Left, Thumbnail } from 'native-base'
 import header from './common/header'
 import { connect } from 'react-redux'
 import { ContainerBackground } from './common/ContainerBackground'
@@ -41,8 +39,8 @@ function mapStateToProps(state){
     let score = state.gamify.score
     let listGarbage = state.track.garbageCoordinates
     let location = Object.assign({}, state.track.userCoordinates, {
-      latitudeDelta: 0.0922,
-      longitudeDelta: 0.0421,
+      latitudeDelta: 0.5000,
+      longitudeDelta: 0.5000,
     })
     console.log(location)
     return {

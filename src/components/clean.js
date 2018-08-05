defff46ad81d11ae81b2a46d0c8d8336aee1629f
kanekotic <alvarojosepl@gmail.com>
2018-08-05 20:55:07 +0200

    added carousel with garbage previews

--- src/components/clean.js
+++ src/components/clean.js
@@ -1,8 +1,45 @@
 import React, { Component } from 'react'
+import { StyleSheet, Dimensions, View, Text } from 'react-native'
+import { Left, Body, Right, Icon, Thumbnail, ListItem } from 'native-base'
 import header from './common/header'
 import { connect } from 'react-redux'
 import { ContainerBackground } from './common/ContainerBackground'
 import MapView, { Marker } from 'react-native-maps'
+import Carousel from 'react-native-snap-carousel'
+
+const horizontalMargin = 20;
+const slideWidth = 280;
+
+const sliderWidth = Dimensions.get('window').width;
+const itemWidth = slideWidth + horizontalMargin * 2;
+const itemHeight = 100;
+
+var styles = StyleSheet.create({
+    slide: {
+        width: itemWidth,
+        height: itemHeight,
+        paddingHorizontal: horizontalMargin
+        // other styles for the item container
+    },
+    slideInnerContainer: {
+        width: slideWidth,
+        flex: 1
+        // other styles for the inner container
+    },
+    fullScreen: {
+        flex:1
+    },
+    floatView: {
+        position: 'absolute',
+        bottom: 10,
+        height: itemHeight,
+        left: horizontalMargin,
+        right: horizontalMargin,
+    },
+    parent: {
+        flex: 1
+    }
+});
 
 class Clean extends Component {
     constructor(props){
@@ -10,26 +47,58 @@ class Clean extends Component {
         this.props.updateGarbageList()
     }
 
+    _renderItem ({item, _}, props) {
+        return (
+            <View>
+                <ListItem thumbnail>
+                    <Left>
+                        <Thumbnail square source={require('./resources/logo.png')} />
+                    </Left>
+                    <Body>
+                        <Text>latitude: {item.latitude}</Text>
+                        <Text>longitude: {item.longitude}</Text>
+                    </Body>
+                    <Right>
+                        <Icon name="trash" onPress={() => {
+                            props.sendClean(item.id)
+                            props.updateScore(10)}} />
+                        </Right>
+                </ListItem>
+            </View>
+        );
+    }
+
     render() {
         return (
             <ContainerBackground>
                 {header("Clean", this.props.score)}
-                <MapView style={{flex: 1}}
+                <View style={styles.fullScreen}>
+                <MapView style={styles.fullScreen}
+                    initialRegion={this.props.location}
                     region={this.props.location}
                     showsUserLocation = {true}
                     showsPointsOfInterest = {false}
                     zoomEnabled = {true}
                     scrollEnabled = {true}
-                    showsMyLocationButton = {true}>
-                    {
-                        this.props.listGarbage.map((element, index) => 
-                        <Marker key={"element_"+index}
-                        coordinate={element}
-                        title="some garbage"
-                        description="some garbage tags"
-                        />
-                    )}
-                </MapView>
+                    showsMyLocationButton = {true}/>
+                </View>
+                <View style={styles.floatView}>
+                <Carousel
+                  ref={c => this._slider1Ref = c}
+                  data={this.props.listGarbage}
+                  renderItem={param => this._renderItem(param, this.props)}
+                  sliderWidth={sliderWidth}
+                  itemWidth={itemWidth}
+                  firstItem={1}
+                  inactiveSlideScale={0.94}
+                  inactiveSlideOpacity={0.7}
+                  containerCustomStyle={styles.slider}
+                  contentContainerCustomStyle={styles.sliderContentContainer}
+                  loop={true}
+                  loopClonesPerSide={1}
+                    />
+                </View>
+
             </ContainerBackground>
         )
     }
@@ -37,7 +106,7 @@ class Clean extends Component {
 
 function mapStateToProps(state){
     let score = state.gamify.score
-    let listGarbage = state.track.garbageCoordinates
+    let listGarbage = state.track.garbageCoordinates.toJS()
     let location = Object.assign({}, state.track.userCoordinates, {
       latitudeDelta: 0.5000,
       longitudeDelta: 0.5000,
